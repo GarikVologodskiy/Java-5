074aa0563e7fcb4f0f678dbdca8a28051bd8172a
GarikVologodskiy <Bavaria22001>
2019-04-23 14:38:17 +0300

    Cycles and Array

--- src/com/main/company/Main.java
+++ src/com/main/company/Main.java
@@ -1,5 +1,4 @@
 package com.main.company;
-
 import java.math.BigInteger;
 
 public class Main {
@@ -9,7 +8,7 @@ public class Main {
         var x1 = 10;
         int x = Integer.MAX_VALUE;
         System.out.println("x = " + x);
-        System.out.println("Integer.MIN_VALUE = " + Integer.MIN_VALUE );       
+        System.out.println("Integer.MIN_VALUE = " + Integer.MIN_VALUE);
 
         long y = Long.MAX_VALUE;
         System.out.println("Long.MAX_VALUE = " + y);
@@ -35,19 +34,47 @@ public class Main {
         System.out.println("c = " + c);
 
         int a1 = ++x1; //a1 = 11
-
         //int a1 = x1++; //a1 = 10
         System.out.println("a1 = " + a1);
 
-
-
         //boolean b = true;
         //boolean b1 = false;
+
+/* do while */
+        //do {
+        //  System.out.println("Hello");
+        //a++;
+        //} while (a < 10);
+
+        //}
+        // }
+/*for */
+        //for (x = 0; x < 10; x += 1) {
+          //  System.out.println(x);
+            //if (x > 10)
+              //  x += x;
+
+/* foreach array */
+        //int[] arr = {1,2,3,4,5,6,7,8,9};
+        //for (x = 0; x < arr.length; x ++){
+          //  System.out.println("arr = " + arr[x]);
+
+        /* array */
+        //int[]room;
+
+        //for(expr1;expr2;expr3) {
+          //  body1
+        //if (expr4) continue;
+          //body2
+        //}
+
+        String s1 = "WhereI.m)";
+        String s2 =  new String ("WhereI.m)");
+        String s3 = s2.intern();
+        //System.out.println(s1.charAt(0));
+        //System.out.println(s1.charAt(1));
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s2 == s3);
+                    }
     }
-}
-//var x = 0;
-//while(x<100):{
-//for(x = 0; x < 10; x++) {
-//  System.out.println(x);
-//}
-//}
