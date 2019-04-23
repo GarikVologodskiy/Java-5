2fd9f5e239776b3e9e849a6566b2ea18e4df7849
GarikVologodskiy <Bavaria2001>
2019-04-23 22:02:57 +0500

    Always Winning Roulette (AWR)

--- src/com/main/company/Slot1.java
+++ src/com/main/company/Slot1.java
@@ -1,24 +1,23 @@
-package com.main.company;
+package com.companyMet;
 
-public class Slot1 {
-
+public class Main {
     public static void main (String... __) {
 
-        final int COLBAR = {0,0,0};
-        final int SIZE = 5;
+        final int[] COLBAR = {0,0,0};
+        final int SIZE = 7;
         final int MAX_VALUE = 100;
         final int BET = 10;
 
-        final int money = 100;
+        int money1 = 100;
         int barPos1 = 0;
         int barPos2 = 0;
         int barPos3 = 0;
 
-        while ((money -= BET) >= 0) {
-            System.out.println("money = " + money);
+        while ((money1 -= BET) >= 0) {
+            System.out.println("money = " + money1);
             int value = (int) Math.round(Math.random() * MAX_VALUE);
             if (value == 0) {
-                money += BET;
+                money1 += BET;
                 continue;
             }
             COLBAR[0] = (COLBAR[0] + value) % SIZE;
@@ -29,14 +28,14 @@ public class Slot1 {
             //COLBAR[2] = value * 4 % SIZE + (int) Math.round(Math.random() *10);
 
             System.out.println("Выпало " + COLBAR[0] + "" + COLBAR[1] + "" + COLBAR[2]);
-            if (COLBAR[0] == COLBAR[1] && COLBAR[0] == COLBAR[3]) {
-                money += 1_000;
-                System.out.println("Выгрышь");
+            if (COLBAR[0] == COLBAR[1] && COLBAR[0] == COLBAR[2]) {
+                money1 += 1_000;
+                System.out.println("Выигрышь");
                 break;
             }   else
                 System.out.println("Проигрышь");
 
         }
-        System.out.println("money = " + money);
+        System.out.println("money = " + money1);
     }
 }
