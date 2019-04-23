e5d956c9ab721789b35163a8cb69b8aed0a76ed9
GarikVologodskiy <Bavaria22001>
2019-04-23 09:07:33 +0300

    Java-2. About for.

---
+++ src/com/main/company/Main.java
@@ -0,0 +1,28 @@
+package com.main.company;
+
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = Integer.MAX_VALUE;
+        System.out.println("x = " + x);
+        System.out.println("Integer.MIN_VALUE = " + Integer.MIN_VALUE );       
+        long y = Long.MAX_VALUE;
+        System.out.println("Long.MAX_VALUE = " + y);
+
+        Short z = Short.MAX_VALUE;
+        System.out.println("Short.MAX_VALUE = " + z);
+
+        Double d = Double.MAX_VALUE;
+        System.out.println("Double.MAX_VALUE " + d);
+
+        Float f = Float.MAX_VALUE;
+        System.out.println("Float.MAX_VALUE " + f);
+    }
+}
+
+//var x = 0;
+//for(x = 0; x < 10; x++) {
+//  System.out.println(x);
+//}
