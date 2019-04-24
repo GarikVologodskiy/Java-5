b8a7bf170d439685c016c98ee249ecc77bc4f89f
GarikVologodskiy <Bavaria22001>
2019-04-24 09:47:21 +0300

    Spinning drum_02

--- src/com/main/company/Slot1.java
+++ src/com/main/company/Slot1.java
@@ -1,17 +1,17 @@
 package com.companyMet;
 
-public class Main {
+public class Slot1 {
     public static void main (String... __) {
 
         final int[] COLBAR = {0,0,0};
-        final int SIZE = 7;
+        final int SIZE = 5;
         final int MAX_VALUE = 100;
         final int BET = 10;
 
         int money1 = 100;
-        int barPos1 = 0;
-        int barPos2 = 0;
-        int barPos3 = 0;
+        //int barPos1 = 0;
+        //int barPos2 = 0;
+        //int barPos3 = 0;
 
         while ((money1 -= BET) >= 0) {
             System.out.println("money = " + money1);
@@ -20,9 +20,12 @@ public class Main {
                 money1 += BET;
                 continue;
             }
-            COLBAR[0] = (COLBAR[0] + value) % SIZE;
-            COLBAR[1] = (COLBAR[1] + value) % SIZE;
-            COLBAR[2] = (COLBAR[2] + value) % SIZE;
+            COLBAR[0] = value % SIZE + (int) Math.round(Math.random());
+            COLBAR[1] = value * 2 % SIZE + (int) Math.round(Math.random());
+            COLBAR[2] = value * 4 % SIZE + (int) Math.round(Math.random());
+            //COLBAR[0] = (COLBAR[0] + value) % SIZE;
+            //COLBAR[1] = (COLBAR[1] + value) % SIZE;
+            //COLBAR[2] = (COLBAR[2] + value) % SIZE;
             //COLBAR[0] = value % SIZE + (int) Math.round(Math.random() *10);
             //COLBAR[1] = value * 2 % SIZE + (int) Math.round(Math.random() *10);
             //COLBAR[2] = value * 4 % SIZE + (int) Math.round(Math.random() *10);
@@ -30,10 +33,10 @@ public class Main {
             System.out.println("Выпало " + COLBAR[0] + "" + COLBAR[1] + "" + COLBAR[2]);
             if (COLBAR[0] == COLBAR[1] && COLBAR[0] == COLBAR[2]) {
                 money1 += 1_000;
-                System.out.println("Выигрышь");
+                System.out.println("Выигрышь!");
                 break;
             }   else
-                System.out.println("Проигрышь");
+                System.out.println("Проигрышь...");
 
         }
         System.out.println("money = " + money1);
